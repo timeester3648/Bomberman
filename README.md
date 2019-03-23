0d8301ce21d4924fd947bb9aa0d794bda8d8dfa2
timeester3648 <weteringt@gmail.com>
2019-03-23 10:21:44 +0100

    Update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Bomberman
 
 A clone of the game "Bomberman" I made in C++. 
+
+Is it easy to make a cheat for this? yes.
+Could i easily fix it? yes.
+Will I do it? no.
--- TestMod/TestMod.cpp
+++ TestMod/TestMod.cpp
@@ -2,12 +2,8 @@
 
 MOD_API void registerMod() {
 
-	MessageBoxA(NULL, "register", "Message", MB_OK);
-
 }
 
 MOD_API void unregisterMod() {
-
-	MessageBoxA(NULL, "unregister", "Message", MB_OK);
 
 }
